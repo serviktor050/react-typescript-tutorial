38167deb924f191819809323da0c7ab70898fea0
serviktor050 <mr.s-gagulin@yandex.ru>
2021-10-05 16:20:57 +0300

    Реализовано приложение со списком дел

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Navbar } from "./components/Navbar";
+import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { ITodo } from "./interfaces";
 
-function App() {
+declare var confirm: (question: string) => boolean;
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem("todos") || "[]") as ITodo[];
+    setTodos(saved);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addHandler = (title: string) => {
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false,
+    };
+    // setTodos([newTodo, ...todos]);
+    setTodos((prev) => [newTodo, ...prev]);
+  };
+
+  const toggleHandler = (id: number) => {
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const removeHandler = (id: number) => {
+    const shouldRemove = confirm("Вы уверены, что хотите удалить элемент?");
+    if (shouldRemove) {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1>Test</h1>
+        <TodoForm onAdd={addHandler} />
+        <TodoList
+          todos={todos}
+          onToggle={toggleHandler}
+          onRemove={removeHandler}
+        />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
